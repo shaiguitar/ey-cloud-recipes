2df3eee599836b770968da2daa48f11d981cffbc
Shai <shaiguitar@gmail.com>
2010-02-24 15:45:33 +0200

    rack stuffin

--- cookbooks/uninstall_rack/recipes/default.rb
+++ cookbooks/uninstall_rack/recipes/default.rb
@@ -1,5 +1,15 @@
-execute "testing" do
-  command %Q{
-  gem uninstall rack -v 1.1.0 ; gem uninstall rack -v 1.0.0
-  }
+#execute "testing" do
+#  command %Q{
+#  gem uninstall rack -v 1.1.0 ; gem uninstall rack -v 1.0.0
+#  }
+#end
+#
+
+gem_package "rack" do 
+   action :remove
+end
+
+gem_package "rack" do 
+  action :install
+  version "1.0.0"
 end
